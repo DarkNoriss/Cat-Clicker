665ad767bcecc04098466d7db9b08d58daa64d85
DarkNoriss <darknoriss@gmail.com>
2022-11-29 18:02:58 +0100

    new building script using more unity things

--- Cat Clicker/Assets/Scripts/Building.cs
+++ Cat Clicker/Assets/Scripts/Building.cs
@@ -4,10 +4,30 @@ using TMPro;
 
 public class Building : MonoBehaviour
 {
-    public string buildingName { get; set; }
-    public double buildingCost { get; set; }
-    public double buildingDefaultCPS { get; set; }
+    public string buildingName;
+    public double buildingCost;
+    public double buildingDefaultCPS;
 
     private int buildingAmount;
     private double buildingCPS;
+
+    [SerializeField]
+    private TextMeshProUGUI buildingTextName;
+
+    [SerializeField]
+    private TextMeshProUGUI buildingTextCost;
+
+    [SerializeField]
+    private TextMeshProUGUI buildingTextAmount;
+
+    public void Awake()
+    {
+        buildingTextName.text = buildingName;
+        buildingTextCost.text = "" + buildingCost;
+    }
+
+    public void BuildingBuy()
+    {
+        print("BUYING " + buildingName);
+    }
 }
