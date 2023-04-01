a2e410623073f91b9558647384cd65b00e996ec7
DarkNoriss <darknoriss@gmail.com>
2023-04-01 06:07:34 +0200

    basic functions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
+import { useEffect, useState } from 'react';
+import { loadData } from '../utils/localData';
 import { Header } from './Header';
+import { Data } from '../utils/types';
 
 export const App = () => {
-  console.log('hey typescript!');
+  const [data] = useState<Data[]>(loadData());
+  const [selectedCat, setSelectedCat] = useState<number>(data[0].cats);
+
   return (
     <>
       <Header />
       <div className="app"></div>
-      {/* <Footer /> */}
     </>
   );
 };
