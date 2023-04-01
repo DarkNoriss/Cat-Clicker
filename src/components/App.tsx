9f0f5807e64d83450ca24cf6d21ee9658d605325
DarkNoriss <darknoriss@gmail.com>
2023-04-01 06:55:20 +0200

    updating and saving with localstorage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from 'react';
-import { loadData } from '../utils/localData';
+import { useState } from 'react';
+import { loadData, saveData } from '../utils/localData';
 import { Header } from './Header';
 import { Data } from '../utils/types';
+import { Cat } from './Cat';
+import { Display } from './Display';
+import { Store } from './Store';
+import '../styles/app.scss';
+import { useUpdateEffect } from '../utils/useUpdateEffect';
 
 export const App = () => {
-  const [data] = useState<Data[]>(loadData());
-  const [selectedCat, setSelectedCat] = useState<number>(data[0].cats);
+  const [data, setData] = useState<Data[]>(loadData());
+  const [cats, setCats] = useState<number>(data[0].cats);
+
+  useUpdateEffect(() => {
+    saveData(data);
+  }, [data]);
+
+  useUpdateEffect(() => {
+    const updatedData = [...data];
+    updatedData[0].cats = cats;
+    setData(updatedData);
+  }, [cats]);
 
   return (
     <>
       <Header />
-      <div className="app"></div>
+      <div className="app">
+        <Cat cats={cats} addCats={setCats} />
+        <Display />
+        <Store />
+      </div>
     </>
   );
 };
