fa33eeb909325717432a152a98a956856fceb1f5
DarkNoriss <darknoriss@gmail.com>
2023-04-01 06:56:03 +0200

    saving to localstorage

--- src/utils/localData.tsx
+++ src/utils/localData.tsx
@@ -1,17 +1,13 @@
 import { LOCAL_STORAGE_KEY } from '../constants';
-import { Data } from './types';
-
-export const loadDataCats = () => {
-  console.log('Loading local storage...');
-};
+import { Data, Props } from './types';
 
 export const loadData = (): Data[] => {
   const storedTasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? 'null') as Data[];
   return storedTasks || emptyData();
 };
 
-export const saveData = () => {
-  console.log('Saving to local storage...');
+export const saveData = (data: Data[]) => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
 };
 
 const emptyData = (): Data[] => {
