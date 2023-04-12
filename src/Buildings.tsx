9f9a75d05c4bbefa7af34d96e989d6327bd4e11b
DarkNoriss <darknoriss@gmail.com>
2023-04-12 22:39:38 +0200

    more variable to buildings

--- src/Buildings.tsx
+++ src/Buildings.tsx
@@ -1,52 +1,48 @@
 export type BuildingsType = {
   name: string;
   desc: string;
+  icon: string;
   priceDef: number;
   perSecondDef: number;
-  icon: string;
-  amount: number;
-  perSecond: number;
+  amount?: number;
+  perSecond?: number;
+  bonus?: number;
+  unlocked?: boolean;
+  discovered?: boolean;
 };
 
 export const catPaw = {
   name: 'Paw',
   desc: 'meow meow',
+  icon: 'catPaw.png',
   priceDef: 15,
   perSecondDef: 0.2,
-  icon: 'catPaw.png',
-  amount: 0,
-  perSecond: 0,
 };
 
 export const catBakery = {
   name: 'Bakery',
   desc: 'meow meow',
+  icon: 'catBakery.png',
   priceDef: 100,
   perSecondDef: 1,
-  icon: 'catBakery.png',
-  amount: 0,
-  perSecond: 0,
 };
 
 export const catFarmer = {
   name: 'Farmer',
   desc: 'meow meow',
+  icon: 'catFarmer.png',
   priceDef: 1100,
   perSecondDef: 8,
-  icon: 'catFarmer.png',
-  amount: 0,
-  perSecond: 0,
 };
 
 export const catMiner = {
   name: 'Miner',
   desc: 'meow meow',
+  icon: 'catMiner.png',
   priceDef: 12000,
   perSecondDef: 47,
-  icon: 'catMiner.png',
-  amount: 0,
-  perSecond: 0,
 };
+
 //   catWorket: 0,
 //   catBanker: 0,
 //   catTemple: 0,
