3e0163dd58f338850d37ff4e3539acf2fd1d9368
DarkNoriss <darknoriss@gmail.com>
2023-04-12 22:41:17 +0200

    added unlocked, discovered

--- src/components/CreateBuilding.tsx
+++ src/components/CreateBuilding.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { PRICE_MULTIPLIER, PATHBUILDINGS } from '../constants';
 import { useUpdateEffect } from '../utils/useUpdateEffect';
 import { BuildingsType } from '../Buildings';
@@ -6,13 +6,26 @@ import { BuildingsType } from '../Buildings';
 type BuildingProps = {
   building: BuildingsType;
   index: number;
+  buildingList: BuildingsType[];
   setBuildings: React.Dispatch<React.SetStateAction<BuildingsType[]>>;
 };
 
-export const CreateBuilding: React.FC<BuildingProps> = ({ building, index, setBuildings }) => {
+export const CreateBuilding: React.FC<BuildingProps> = ({
+  building,
+  index,
+  buildingList,
+  setBuildings,
+}) => {
   const [price, setPrice] = useState<number>(building.priceDef);
-  const [perSecond, setPerSecond] = useState<number>(0);
-  const [amount, setAmount] = useState<number>(building.amount);
+  const [perSecond, setPerSecond] = useState<number>(building.perSecond ?? 0);
+  const [amount, setAmount] = useState<number>(building.amount ?? 0);
+  const [bonus, setBonus] = useState<number>(building.bonus ?? 1);
+  const unlockedRef = useRef<boolean>(building.unlocked ?? false);
+  const discoveredRef = useRef<boolean>(building.discovered ?? false);
+
+  const notDiscovered = {
+    filter: 'brightness(0)',
+  };
 
   useUpdateEffect(() => {
     calcPrice();
@@ -23,10 +36,26 @@ export const CreateBuilding: React.FC<BuildingProps> = ({ building, index, setBu
     updateBuildings();
   }, [perSecond]);
 
+  useUpdateEffect(() => {
+    if (!unlockedRef.current) shouldUnlock();
+    if (!discoveredRef.current) shouldDiscover();
+  }, [buildingList]);
+
   const updateBuildings = () => {
+    // converting React.MutableRefObject<boolean> to boolean
+    const unlocked = unlockedRef.current;
+    const discovered = discoveredRef.current;
+
     setBuildings((prevBuildings) => {
       const newBuildings = [...prevBuildings];
-      newBuildings[index] = { ...newBuildings[index], perSecond, amount };
+      newBuildings[index] = {
+        ...newBuildings[index],
+        perSecond,
+        amount,
+        bonus,
+        unlocked,
+        discovered,
+      };
       return newBuildings;
     });
   };
@@ -34,13 +63,25 @@ export const CreateBuilding: React.FC<BuildingProps> = ({ building, index, setBu
   const calcPrice = () => {
     if (amount === 0) return;
     const newPrice = building.priceDef * Math.pow(PRICE_MULTIPLIER, amount);
-    setPrice(newPrice);
+    const newPriceRounded = Math.round(newPrice * 100) / 100;
+    setPrice(newPriceRounded);
   };
 
   const calcPerSecond = () => {
     if (amount === 0) return;
-    const newPerSecond = building.perSecondDef * amount;
+    const newPerSecond = building.perSecondDef * bonus * amount;
     setPerSecond(newPerSecond);
+  };
+
+  const shouldUnlock = () => {
+    if (index === 0) return (unlockedRef.current = true);
+    const prevOwned = buildingList[index - 1].amount;
+    if (prevOwned !== undefined && prevOwned > 0) return (unlockedRef.current = true);
+  };
+
+  const shouldDiscover = () => {
+    // to do
+    // if price is > cats
   };
 
   const handleBuy = () => {
@@ -48,16 +89,24 @@ export const CreateBuilding: React.FC<BuildingProps> = ({ building, index, setBu
   };
 
   return (
-    <div className="h-16 flex cursor-pointer " onClick={handleBuy}>
-      <img src={`${PATHBUILDINGS}${building.icon}`} className="w-16 aspect-square"></img>
+    <>
+      {unlockedRef.current && (
+        <div className="h-16 flex cursor-pointer " onClick={handleBuy}>
+          <img
+            src={`${PATHBUILDINGS}${building.icon}`}
+            className="w-16 aspect-square"
+            style={discoveredRef ? notDiscovered : {}}
+          ></img>
 
-      <div className="flex-1 flex flex-col m-2 items-start">
-        <p>{building.name}</p>
-        <p>{Math.round(price * 100) / 100}</p>
-      </div>
-      <div className="flex items-end m-2">
-        <p>{amount}</p>
-      </div>
-    </div>
+          <div className="flex-1 flex flex-col m-2 items-start">
+            <p>{building.name}</p>
+            <p>{price}</p>
+          </div>
+          <div className="flex items-end m-2">
+            <p>{amount}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
