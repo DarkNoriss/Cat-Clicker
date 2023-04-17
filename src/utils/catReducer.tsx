c048bfbe601dcef3fe33d734907c612cb45398c8
Patryk Przybylski <darknoriss@gmail.com>
2023-04-17 19:57:34 +0200

    reducer using immer

--- src/utils/catReducer.tsx
+++ src/utils/catReducer.tsx
@@ -1,3 +1,17 @@
-export const catReducer = (state: any, action: any) => {
-  return state;
+import { CatDataType } from './emptyData';
+import { produce } from 'immer';
+
+export const catReducer = (state: CatDataType, action: any) => {
+  switch (action.type) {
+    case 'add_cat':
+      return produce(state, (draftState) => {
+        draftState.cats.amount += draftState.cats.perClick;
+      });
+    case 'buy_building':
+      console.log('buy_building');
+      return state;
+    default:
+      console.log('no type');
+      return state;
+  }
 };
