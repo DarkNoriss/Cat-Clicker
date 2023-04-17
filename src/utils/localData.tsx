35ce6286843f77ba4b733cf627958517df143646
Patryk Przybylski <darknoriss@gmail.com>
2023-04-17 19:57:23 +0200

    removing consolelog

--- src/utils/localData.tsx
+++ src/utils/localData.tsx
@@ -2,12 +2,10 @@ import { LOCAL_STORAGE_KEY } from '../constants';
 import { CatDataType, getInitialData } from './emptyData';
 
 export const loadData = (): CatDataType => {
-  // console.log('LOADING DATA');
   const storedTasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? 'null');
   return storedTasks || getInitialData();
 };
 
 export const saveData = (data: CatDataType) => {
-  // console.log('SAVING DATA');
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
 };
