446b573c244f8f0c324d269a93ad39e6eb532707
Patryk Przybylski <darknoriss@gmail.com>
2023-04-18 14:13:06 +0200

    main game loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { Dispatch, createContext, useReducer, useState } from 'react';
+import { createContext, useEffect, useReducer, useRef } from 'react';
 import { loadData, saveData } from './utils/localData';
 import { Cat } from './components/Cats';
 import { Display } from './components/Display';
 import { Store } from './components/Store';
 import { useUpdateEffect } from './utils/useUpdateEffect';
-import { CatDataType, CatsType } from './utils/emptyData';
 import { catReducer } from './utils/catReducer';
 
 export const CatGameContext = createContext({});
 
 export const App = () => {
   const [catGame, dispatchCatGame] = useReducer(catReducer, loadData());
+  const callback = useRef();
+
+  useEffect(() => {
+    callback.current = () => {
+      dispatchCatGame({
+        type: 'game_loop',
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      callback.current();
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   useUpdateEffect(() => saveData(catGame), [catGame]);
 
