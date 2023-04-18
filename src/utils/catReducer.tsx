546a6bdbff8db51b8bed50f61638e8b22393f60a
Patryk Przybylski <darknoriss@gmail.com>
2023-04-18 14:11:58 +0200

    reducer

--- src/utils/catReducer.tsx
+++ src/utils/catReducer.tsx
@@ -1,15 +1,62 @@
+import { PRICE_MULTIPLIER } from '../constants';
 import { CatDataType } from './emptyData';
 import { produce } from 'immer';
 
 export const catReducer = (state: CatDataType, action: any) => {
   switch (action.type) {
-    case 'add_cat':
+    case 'game_loop':
       return produce(state, (draftState) => {
-        draftState.cats.amount += draftState.cats.perClick;
+        const cats = draftState.cats;
+        cats.amount += draftState.cats.perSecond;
       });
-    case 'buy_building':
-      console.log('buy_building');
-      return state;
+
+    case 'cat_add':
+      return produce(state, (draftState) => {
+        const cats = draftState.cats;
+        cats.amount += draftState.cats.perClick;
+      });
+
+    case 'cat_set_persecond':
+      return produce(state, (draftState) => {
+        const cats = draftState.cats;
+        cats.perSecond = action.payload;
+      });
+
+    case 'building_buy':
+      return produce(state, (draftState) => {
+        const cats = draftState.cats;
+        const building = draftState.buildings[action.payload];
+        if (cats.amount >= building.price) {
+          cats.amount -= building.price;
+          building.amount += 1;
+          building.price = building.priceDef * Math.pow(PRICE_MULTIPLIER, building.amount);
+          building.perSecond = building.perSecondDef * building.bonus * building.amount;
+        }
+      });
+
+    case 'building_unlock':
+      return produce(state, (draftState) => {
+        const building = draftState.buildings[action.payload];
+        building.unlocked = true;
+      });
+
+    case 'building_discover':
+      return produce(state, (draftState) => {
+        const building = draftState.buildings[action.payload];
+        building.discovered = true;
+      });
+
+    case 'building_add_variable':
+      return produce(state, (draftState) => {
+        const building = draftState.buildings[action.payload];
+        building.amount = 0;
+        building.price = building.priceDef;
+        building.perSecond = 0;
+        building.bonus = 1;
+        action.payload === 0 ? (building.unlocked = true) : (building.unlocked = false);
+        building.discovered = false;
+      });
+
     default:
       console.log('no type');
       return state;
