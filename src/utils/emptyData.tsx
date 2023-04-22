0bf66e7e8a0d1ba66316b16c029631fcb26b21d3
Patryk Przybylski <darknoriss@gmail.com>
2023-04-22 05:35:04 +0200

    removing undefined types

--- src/utils/emptyData.tsx
+++ src/utils/emptyData.tsx
@@ -8,14 +8,14 @@ export type BuildingsType = {
   name: string;
   desc: string;
   icon: string;
-  priceDef: number | bigint;
+  priceDef: number;
   perSecondDef: number;
-  amount?: number;
-  price?: number | bigint;
-  perSecond?: number;
-  bonus?: number;
-  unlocked?: boolean;
-  discovered?: boolean;
+  amount: number;
+  price: number;
+  perSecond: number;
+  bonus: number;
+  unlocked: boolean;
+  discovered: boolean;
 };
 
 export type CatDataType = {
@@ -24,7 +24,9 @@ export type CatDataType = {
   buildings: BuildingsType[];
 };
 
-export const getInitialData = (): CatDataType => {
+export const getInitialData = (): Omit<CatDataType, 'buildings'> & {
+  buildings: Partial<BuildingsType>[];
+} => {
   return {
     cats: {
       amount: 0,
@@ -133,41 +135,41 @@ export const getInitialData = (): CatDataType => {
         priceDef: 2_100_000_000_000_000,
         perSecondDef: 2_900_000_000,
       },
-      {
-        name: 'Chancemaker',
-        desc: 'meow meow',
-        icon: 'catChancemaker',
-        priceDef: 26_000_000_000_000_000n,
-        perSecondDef: 21_000_000_000,
-      },
-      {
-        name: 'Fractal Engine',
-        desc: 'meow meow',
-        icon: 'catFractalEngine',
-        priceDef: 310_000_000_000_000_000n,
-        perSecondDef: 150_000_000_000,
-      },
-      {
-        name: 'Javascript Console',
-        desc: 'meow meow',
-        icon: 'catJavascriptConsole',
-        priceDef: 71_000_000_000_000_000_000n,
-        perSecondDef: 1_100_000_000_000,
-      },
-      {
-        name: 'Idleverse',
-        desc: 'meow meow',
-        icon: 'catIdleverse',
-        priceDef: 12_000_000_000_000_000_000_000n,
-        perSecondDef: 8_300_000_000_000,
-      },
-      {
-        name: 'Cortex Baker',
-        desc: 'meow meow',
-        icon: 'catCortexBaker',
-        priceDef: 1_900_000_000_000_000_000_000_000n,
-        perSecondDef: 64_000_000_000_000,
-      },
+      // {
+      //   name: 'Chancemaker',
+      //   desc: 'meow meow',
+      //   icon: 'catChancemaker',
+      //   priceDef: 26_000_000_000_000_000n,
+      //   perSecondDef: 21_000_000_000n,
+      // },
+      // {
+      //   name: 'Fractal Engine',
+      //   desc: 'meow meow',
+      //   icon: 'catFractalEngine',
+      //   priceDef: 310_000_000_000_000_000n,
+      //   perSecondDef: 150_000_000_000n,
+      // },
+      // {
+      //   name: 'Javascript Console',
+      //   desc: 'meow meow',
+      //   icon: 'catJavascriptConsole',
+      //   priceDef: 71_000_000_000_000_000_000n,
+      //   perSecondDef: 1_100_000_000_000n,
+      // },
+      // {
+      //   name: 'Idleverse',
+      //   desc: 'meow meow',
+      //   icon: 'catIdleverse',
+      //   priceDef: 12_000_000_000_000_000_000_000n,
+      //   perSecondDef: 8_300_000_000_000n,
+      // },
+      // {
+      //   name: 'Cortex Baker',
+      //   desc: 'meow meow',
+      //   icon: 'catCortexBaker',
+      //   priceDef: 1_900_000_000_000_000_000_000_000n,
+      //   perSecondDef: 64_000_000_000_000n,
+      // },
     ],
   };
 };
